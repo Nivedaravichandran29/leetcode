5cdcfe52b988133c1c8bf81c6f88911e25f20c7d
Nivedaravichandran29 <rniveda81@gmail.com>
2026-07-17 15:06:04 +0530

    Create 1153-ProductSalesAnalysisI

---
+++ 1153-ProductSalesAnalysisI/1153-ProductSalesAnalysisI.sql
@@ -0,0 +1,4 @@
+-- Last updated: 7/17/2026, 3:06:01 PM
+# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
+SELECT pd.product_name, sl.year, sl.price FROM Sales AS sl INNER JOIN Product AS pd ON sl.product_id = pd.product_id;
