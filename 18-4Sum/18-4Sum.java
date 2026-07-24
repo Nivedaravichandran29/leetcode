94d31324dbd377166da6c11bb16932dd14758846
Nivedaravichandran29 <rniveda81@gmail.com>
2026-07-24 10:24:03 +0530

    File updated at 7/24/2026, 10:24:02 AM

--- 18-4Sum/18-4Sum.java
+++ 18-4Sum/18-4Sum.java
@@ -1,38 +1,21 @@
-// Last updated: 7/24/2026, 10:23:07 AM
+// Last updated: 7/24/2026, 10:24:02 AM
 1class Solution {
-2    public ListNode reverseKGroup(ListNode head, int k) {
-3        ListNode dummy = new ListNode(0);
-4        dummy.next = head;
+2    public int strStr(String haystack, String needle) {
+3        int n = haystack.length();
+4        int m = needle.length();
 5
-6        ListNode prevGroup = dummy;
-7
-8        while (true) {
-9            ListNode kth = prevGroup;
-10
-11            for (int i = 0; i < k && kth != null; i++) {
-12                kth = kth.next;
-13            }
-14
-15            if (kth == null) {
-16                break;
-17            }
-18
-19            ListNode groupNext = kth.next;
-20            ListNode prev = groupNext;
-21            ListNode curr = prevGroup.next;
-22
-23            while (curr != groupNext) {
-24                ListNode temp = curr.next;
-25                curr.next = prev;
-26                prev = curr;
-27                curr = temp;
-28            }
-29
-30            ListNode temp = prevGroup.next;
-31            prevGroup.next = kth;
-32            prevGroup = temp;
-33        }
-34
-35        return dummy.next;
-36    }
-37}
+6        for (int i = 0; i <= n - m; i++) {
+7            int j = 0;
+8
+9            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+10                j++;
+11            }
+12
+13            if (j == m) {
+14                return i;
+15            }
+16        }
+17
+18        return -1;
+19    }
+20}
