28951b6afc13293e4a27f2bd65ac4a9b460e53bb
Nivedaravichandran29 <rniveda81@gmail.com>
2026-07-24 10:22:09 +0530

    File updated at 7/24/2026, 10:22:08 AM

--- 18-4Sum/18-4Sum.java
+++ 18-4Sum/18-4Sum.java
@@ -1,27 +1,30 @@
-// Last updated: 7/24/2026, 10:20:53 AM
+// Last updated: 7/24/2026, 10:22:08 AM
 1class Solution {
-2    public ListNode removeNthFromEnd(ListNode head, int n) {
+2    public ListNode reverseKGroup(ListNode head, int k) {
 3
-4        ListNode dummy = new ListNode(0);
-5        dummy.next = head;
+4        ListNode curr = head;
+5        int count = 0;
 6
-7        ListNode fast = dummy;
-8        ListNode slow = dummy;
-9
-10        // Move fast pointer n+1 steps ahead
-11        for (int i = 0; i <= n; i++) {
-12            fast = fast.next;
-13        }
-14
-15        // Move both pointers
-16        while (fast != null) {
-17            fast = fast.next;
-18            slow = slow.next;
-19        }
-20
-21        // Remove the nth node
-22        slow.next = slow.next.next;
+7        // Check if there are at least k nodes
+8        while (curr != null && count < k) {
+9            curr = curr.next;
+10            count++;
+11        }
+12
+13        // If k nodes exist, reverse them
+14        if (count == k) {
+15            curr = reverseKGroup(curr, k);
+16
+17            while (count-- > 0) {
+18                ListNode temp = head.next;
+19                head.next = curr;
+20                curr = head;
+21                head = temp;
+22            }
 23
-24        return dummy.next;
-25    }
-26}
+24            head = curr;
+25        }
+26
+27        return head;
+28    }
+29}
