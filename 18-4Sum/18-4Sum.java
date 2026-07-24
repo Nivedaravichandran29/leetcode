cfebc7a69f53d75305786de9b004a23cf40164f9
Nivedaravichandran29 <rniveda81@gmail.com>
2026-07-24 10:20:53 +0530

    File updated at 7/24/2026, 10:20:53 AM

--- 18-4Sum/18-4Sum.java
+++ 18-4Sum/18-4Sum.java
@@ -1,56 +1,27 @@
-// Last updated: 7/24/2026, 10:19:59 AM
-1import java.util.*;
-2
-3class Solution {
-4    public List<List<Integer>> fourSum(int[] nums, int target) {
-5        List<List<Integer>> result = new ArrayList<>();
+// Last updated: 7/24/2026, 10:20:53 AM
+1class Solution {
+2    public ListNode removeNthFromEnd(ListNode head, int n) {
+3
+4        ListNode dummy = new ListNode(0);
+5        dummy.next = head;
 6
-7        Arrays.sort(nums);
-8        int n = nums.length;
+7        ListNode fast = dummy;
+8        ListNode slow = dummy;
 9
-10        for (int i = 0; i < n - 3; i++) {
-11
-12            // Skip duplicate values for i
-13            if (i > 0 && nums[i] == nums[i - 1])
-14                continue;
-15
-16            for (int j = i + 1; j < n - 2; j++) {
-17
-18                // Skip duplicate values for j
-19                if (j > i + 1 && nums[j] == nums[j - 1])
-20                    continue;
-21
-22                int left = j + 1;
-23                int right = n - 1;
-24
-25                while (left < right) {
-26
-27                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-28
-29                    if (sum == target) {
-30
-31                        result.add(Arrays.asList(
-32                                nums[i], nums[j], nums[left], nums[right]));
-33
-34                        left++;
-35                        right--;
-36
-37                        // Skip duplicates
-38                        while (left < right && nums[left] == nums[left - 1])
-39                            left++;
-40
-41                        while (left < right && nums[right] == nums[right + 1])
-42                            right--;
-43
-44                    } else if (sum < target) {
-45                        left++;
-46                    } else {
-47                        right--;
-48                    }
-49                }
-50            }
-51        }
-52
-53        return result;
-54    }
-55}
+10        // Move fast pointer n+1 steps ahead
+11        for (int i = 0; i <= n; i++) {
+12            fast = fast.next;
+13        }
+14
+15        // Move both pointers
+16        while (fast != null) {
+17            fast = fast.next;
+18            slow = slow.next;
+19        }
+20
+21        // Remove the nth node
+22        slow.next = slow.next.next;
+23
+24        return dummy.next;
+25    }
+26}
