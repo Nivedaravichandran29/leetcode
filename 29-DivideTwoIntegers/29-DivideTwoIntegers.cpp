846f937cc6b261e229cc809bd182551419a95496
Nivedaravichandran29 <rniveda81@gmail.com>
2026-08-04 09:28:30 +0530

    File updated at 8/4/2026, 9:28:30 AM

--- 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -1,9 +1,24 @@
-// Last updated: 8/4/2026, 9:27:50 AM
+// Last updated: 8/4/2026, 9:28:30 AM
 1class Solution {
 2public:
-3    void nextPermutation(vector<int>& nums) {
-4        if(!next_permutation(nums.begin(), nums.end())) {
-5            sort(nums.begin(), nums.end());
-6        }
-7    }
-8};
+3    int longestValidParentheses(string s) {
+4        stack<int> st;
+5        st.push(-1);
+6        int max_len = 0;
+7
+8        for (int i = 0; i < s.length(); i++) {
+9            if (s[i] == '(') {
+10                st.push(i);
+11            } else {
+12                st.pop();
+13                if (st.empty()) {
+14                    st.push(i);
+15                } else {
+16                    max_len = max(max_len, i - st.top());
+17                }
+18            }
+19        }
+20
+21        return max_len;        
+22    }
+23};
