bd17ffb0f7a15a465dddb8e5adb397e4f7479d69
Nivedaravichandran29 <rniveda81@gmail.com>
2026-09-24 22:28:22 +0530

    File updated at 9/24/2026, 10:28:21 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.py
+++ 115-DistinctSubsequences/115-DistinctSubsequences.py
@@ -1,24 +1,10 @@
-# Last updated: 9/24/2026, 10:22:47 PM
+# Last updated: 9/24/2026, 10:28:21 PM
 1class Solution:
-2    def wordPattern(self, pattern, s):
-3        words = s.split()
-4
-5        if len(pattern) != len(words):
-6            return False
-7
-8        char_to_word = {}
-9        word_to_char = {}
-10
-11        for ch, word in zip(pattern, words):
-12            if ch in char_to_word:
-13                if char_to_word[ch] != word:
-14                    return False
-15
-16            if word in word_to_char:
-17                if word_to_char[word] != ch:
-18                    return False
-19
-20            char_to_word[ch] = word
-21            word_to_char[word] = ch
-22
-23        return True
+2    def reverseString(self, s):
+3        left = 0
+4        right = len(s) - 1
+5
+6        while left < right:
+7            s[left], s[right] = s[right], s[left]
+8            left += 1
+9            right -= 1
