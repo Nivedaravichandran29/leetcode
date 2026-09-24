1ff78b5d8d9812cb21ddce5c545cfc9d60eebdc1
Nivedaravichandran29 <rniveda81@gmail.com>
2026-09-24 22:09:22 +0530

    File updated at 9/24/2026, 10:09:20 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.py
+++ 115-DistinctSubsequences/115-DistinctSubsequences.py
@@ -1,31 +1,9 @@
-# Last updated: 9/24/2026, 10:04:51 PM
+# Last updated: 9/24/2026, 10:09:20 PM
 1class Solution:
-2    def wordBreak(self, s, wordDict):
-3        wordSet = set(wordDict)
-4        memo = {}
-5
-6        def dfs(start):
-7            if start == len(s):
-8                return [""]
-9
-10            if start in memo:
-11                return memo[start]
-12
-13            result = []
-14
-15            for end in range(start + 1, len(s) + 1):
-16                word = s[start:end]
-17
-18                if word in wordSet:
-19                    sentences = dfs(end)
-20
-21                    for sentence in sentences:
-22                        if sentence:
-23                            result.append(word + " " + sentence)
-24                        else:
-25                            result.append(word)
-26
-27            memo[start] = result
-28            return result
-29
-30        return dfs(0)
+2    def titleToNumber(self, columnTitle):
+3        result = 0
+4
+5        for ch in columnTitle:
+6            result = result * 26 + (ord(ch) - ord('A') + 1)
+7
+8        return result
