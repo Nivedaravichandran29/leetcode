dcd518c3503fb5c345b9ee9cf4ae933bceddf10e
Nivedaravichandran29 <rniveda81@gmail.com>
2026-09-24 22:41:39 +0530

    File updated at 9/24/2026, 10:41:38 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.py
+++ 115-DistinctSubsequences/115-DistinctSubsequences.py
@@ -1,10 +1,23 @@
-# Last updated: 9/24/2026, 10:40:23 PM
+# Last updated: 9/24/2026, 10:41:38 PM
 1class Solution:
-2    def countSegments(self, s):
-3        count = 0
-4
-5        for i in range(len(s)):
-6            if s[i] != ' ' and (i == 0 or s[i - 1] == ' '):
-7                count += 1
-8
-9        return count
+2    def findAnagrams(self, s, p):
+3        if len(p) > len(s):
+4            return []
+5
+6        p_count = [0] * 26
+7        window = [0] * 26
+8        result = []
+9
+10        for ch in p:
+11            p_count[ord(ch) - ord('a')] += 1
+12
+13        for i in range(len(s)):
+14            window[ord(s[i]) - ord('a')] += 1
+15
+16            if i >= len(p):
+17                window[ord(s[i - len(p)]) - ord('a')] -= 1
+18
+19            if window == p_count:
+20                result.append(i - len(p) + 1)
+21
+22        return result
