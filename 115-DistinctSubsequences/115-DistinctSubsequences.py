0b7ae293f6a272dcbd662231a176b1500b47a355
Nivedaravichandran29 <rniveda81@gmail.com>
2026-09-24 22:16:43 +0530

    File updated at 9/24/2026, 10:16:42 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.py
+++ 115-DistinctSubsequences/115-DistinctSubsequences.py
@@ -1,9 +1,13 @@
-# Last updated: 9/24/2026, 10:09:20 PM
+# Last updated: 9/24/2026, 10:16:42 PM
 1class Solution:
-2    def titleToNumber(self, columnTitle):
-3        result = 0
-4
-5        for ch in columnTitle:
-6            result = result * 26 + (ord(ch) - ord('A') + 1)
+2    def shortestPalindrome(self, s):
+3        if not s:
+4            return ""
+5
+6        rev = s[::-1]
 7
-8        return result
+8        for i in range(len(s)):
+9            if s[:len(s) - i] == rev[i:]:
+10                return rev[:i] + s
+11
+12        return ""
