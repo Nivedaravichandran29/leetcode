815a2056a8582fabcdba660e83d865cdf7713aa5
Nivedaravichandran29 <rniveda81@gmail.com>
2026-09-24 22:04:52 +0530

    File updated at 9/24/2026, 10:04:51 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.py
+++ 115-DistinctSubsequences/115-DistinctSubsequences.py
@@ -1,33 +1,31 @@
-# Last updated: 9/24/2026, 10:03:50 PM
+# Last updated: 9/24/2026, 10:04:51 PM
 1class Solution:
-2    def minCut(self, s):
-3        n = len(s)
-4
-5        palindrome = [[False] * n for _ in range(n)]
-6
-7        for i in range(n):
-8            palindrome[i][i] = True
+2    def wordBreak(self, s, wordDict):
+3        wordSet = set(wordDict)
+4        memo = {}
+5
+6        def dfs(start):
+7            if start == len(s):
+8                return [""]
 9
-10        for length in range(2, n + 1):
-11            for i in range(n - length + 1):
-12                j = i + length - 1
-13
-14                if s[i] == s[j]:
-15                    if length == 2:
-16                        palindrome[i][j] = True
-17                    else:
-18                        palindrome[i][j] = palindrome[i + 1][j - 1]
-19
-20        dp = [0] * n
-21
-22        for i in range(n):
-23            if palindrome[0][i]:
-24                dp[i] = 0
-25            else:
-26                dp[i] = i
-27
-28                for j in range(1, i + 1):
-29                    if palindrome[j][i]:
-30                        dp[i] = min(dp[i], dp[j - 1] + 1)
-31
-32        return dp[n - 1]
+10            if start in memo:
+11                return memo[start]
+12
+13            result = []
+14
+15            for end in range(start + 1, len(s) + 1):
+16                word = s[start:end]
+17
+18                if word in wordSet:
+19                    sentences = dfs(end)
+20
+21                    for sentence in sentences:
+22                        if sentence:
+23                            result.append(word + " " + sentence)
+24                        else:
+25                            result.append(word)
+26
+27            memo[start] = result
+28            return result
+29
+30        return dfs(0)
