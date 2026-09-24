4886e5382fa6e756a014b7584afda71fad7730a5
Nivedaravichandran29 <rniveda81@gmail.com>
2026-09-24 22:20:18 +0530

    File updated at 9/24/2026, 10:20:17 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.py
+++ 115-DistinctSubsequences/115-DistinctSubsequences.py
@@ -1,41 +1,50 @@
-# Last updated: 9/24/2026, 10:18:10 PM
+# Last updated: 9/24/2026, 10:20:17 PM
 1class Solution:
-2    def calculate(self, s):
-3        stack = []
-4        result = 0
-5        number = 0
-6        sign = 1
-7
-8        for ch in s:
-9            if ch.isdigit():
-10                number = number * 10 + int(ch)
-11
-12            elif ch == '+':
-13                result += sign * number
-14                number = 0
-15                sign = 1
-16
-17            elif ch == '-':
-18                result += sign * number
-19                number = 0
-20                sign = -1
-21
-22            elif ch == '(':
-23                stack.append(result)
-24                stack.append(sign)
-25
-26                result = 0
-27                sign = 1
-28
-29            elif ch == ')':
-30                result += sign * number
-31                number = 0
+2    def addOperators(self, num, target):
+3        result = []
+4
+5        def backtrack(index, expression, value, previous):
+6            if index == len(num):
+7                if value == target:
+8                    result.append(expression)
+9                return
+10
+11            for i in range(index, len(num)):
+12                if i > index and num[index] == '0':
+13                    break
+14
+15                current = num[index:i + 1]
+16                current_num = int(current)
+17
+18                if index == 0:
+19                    backtrack(
+20                        i + 1,
+21                        current,
+22                        current_num,
+23                        current_num
+24                    )
+25                else:
+26                    backtrack(
+27                        i + 1,
+28                        expression + "+" + current,
+29                        value + current_num,
+30                        current_num
+31                    )
 32
-33                sign = stack.pop()
-34                previous_result = stack.pop()
-35
-36                result = previous_result + sign * result
-37
-38        result += sign * number
+33                    backtrack(
+34                        i + 1,
+35                        expression + "-" + current,
+36                        value - current_num,
+37                        -current_num
+38                    )
 39
-40        return result
+40                    backtrack(
+41                        i + 1,
+42                        expression + "*" + current,
+43                        value - previous + previous * current_num,
+44                        previous * current_num
+45                    )
+46
+47        backtrack(0, "", 0, 0)
+48
+49        return result
